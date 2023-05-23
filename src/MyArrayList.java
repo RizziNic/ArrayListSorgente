1c46e87b91a07877b3ed40cc2bf1a580da256c78
RizziNic <rizzijunior.nico@gmail.com>
2023-05-23 11:44:35 +0200

    aa

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class MyArrayList {
+public class MyArrayList<T> {
+
+    public T tipo;
+    int lunghezza;
+    int a[] = new int[1];
+
+    public MyArrayList(){
+
+    }
+
+    public void get(int index){
+        for(int i = 0; i< )
+    }
+
+    public String toString(int a[]){
+        a.
+    }
+
+
 }
